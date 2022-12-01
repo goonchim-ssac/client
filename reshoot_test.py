6bda8803a3ba8a53e01a69f7fbe3d5d21d6f84d6
binibin-k <binibinkk@gmail.com>
2022-12-01 13:49:08 +0900

    재촬영된 이미지를 DB Server로 보내는 기능 구현

--- reshoot_test.py
+++ reshoot_test.py
@@ -8,6 +8,10 @@ from func import ImageFile
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
+if 'last' not in st.session_state:
+    st.session_state.last = None
+
+
 barcode = st.text_input("빈 칸에 바코드를 입력해주세요.")
 # print(st.session_state.count)
 
@@ -16,16 +20,24 @@ if len(barcode) == 13:
     captured_image = st.camera_input("카메라")
 
     if captured_image != None:
-        st.session_state.count += 1
+        st.session_state.count += 1 # last 있으면 이 부분 없어도 될 듯?
         st.image(captured_image)
 
-        buffered_stream = image_file.image_to_buffer(captured_image)
-        upload = {'file': buffered_stream}
+        if st.session_state.last == None: # 이전에 촬영된 이미지가 있는 경우 (첫 번째 촬영인 경우)
+            print(f"{st.session_state.count}번째 촬영된 이미지 정보는 {captured_image} 입니다.")
         
-        print(f"{st.session_state.count}번째에 촬영된 이미지 정보인 {upload}가 전송됩니다.")
-        res = requests.post(url="http://127.0.0.1:8000/test", files=upload)
+        else: # 이전에 촬영된 이미지가 있는 경우
+            buffered_stream = image_file.image_to_buffer(captured_image)
+            upload = {'file': buffered_stream}
+            
+            print(f"{st.session_state.count}번째 촬영된 이미지 정보는 {captured_image} 입니다.")
+            print(f"{st.session_state.count-1}번째에 촬영된 이미지 정보인 {captured_image}가 전송됩니다.")
+            # res = requests.post(url="http://127.0.0.1:8000/test", files=upload)
+            
+            # image_file.drop_image(buffered_stream)
+
         
-        image_file.drop_image(buffered_stream)
+        st.session_state.last = captured_image
 
 
 if st.button('등록'):
