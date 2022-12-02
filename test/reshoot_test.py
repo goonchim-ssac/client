e3640c9bd3e1b8624ed33e2d1ec26a1e85129e0e
binibin-k <binibinkk@gmail.com>
2022-12-02 10:52:22 +0900

    여러 가지 시도했던 파일 정리

--- test/reshoot_test.py
+++ test/reshoot_test.py
@@ -30,11 +30,13 @@ if len(barcode) == 13:
             buffered_stream = image_file.image_to_buffer(captured_image)
             upload = {'file': buffered_stream}
             
-            print(f"{st.session_state.count}번째 촬영된 이미지 정보는 {captured_image} 입니다.")
-            print(f"{st.session_state.count-1}번째에 촬영된 이미지 정보인 {st.session_state.last}가 전송됩니다.")
-            # res = requests.post(url="http://127.0.0.1:8000/test", files=upload)
-            
-            # image_file.drop_image(buffered_stream)
+            # print(f"{st.session_state.count}번째 촬영된 이미지 정보는 {captured_image} 입니다.")
+            # print(f"{st.session_state.count-1}번째에 촬영된 이미지 정보인 {st.session_state.last}가 전송됩니다.")
+            res = requests.post(url="http://127.0.0.1:8000/test", files=upload)
+            print(res)
+
+
+            image_file.drop_image(buffered_stream)
 
         
         st.session_state.last = captured_image
